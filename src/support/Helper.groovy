83cac7f921bb19e35f15e616dad929a087f6a918
alpapad <alpapad@gmail.com>
2023-06-03 17:55:04 +0300

    Adapt pipelines

--- src/support/Helper.groovy
+++ src/support/Helper.groovy
@@ -1,119 +1,124 @@
 package support
 
 class Helper implements Serializable {
-    private final def script
-    private final def env1
-    
-    private def  projectVersion = null
-    
-    Helper(def script) {
-        this.script = script
-    }
+	private final def script
 
-    boolean isPr() {
-       return this.script.env.BRANCH_NAME != null && this.script.env.BRANCH_NAME.startsWith("PR-");
-    }
-
-    boolean isTag(){
-        return this.script.env.TAG_NAME != null && !this.script.env.TAG_NAME.isEmpty()
-    }
-
-    boolean isBranch(){
-        return this.script.env.BRANCH_NAME != null && !this.script.env.BRANCH_NAME.isEmpty()
-    }
-    
-    boolean isRelease(){
-        return (this.script.env.BRANCH_NAME == "develop" || this.script.env.BRANCH_NAME == "master")
-    }
-
-    boolean isPublish(){
-        return this.script.env.BRANCH_NAME == "develop" || this.script.env.BRANCH_NAME == "master" || this.isTag()
-    }
-    
-    boolean isSnapshot(){
-        return !(this.isRelease() || this.isPublish())
-    }
-    
-    boolean hasNoFailures(){
-       return  (this.script.currentBuild.result == null || this.script.currentBuild.result == 'SUCCESS')
-    }
-
-    String getReleaseMessage(def nextReleaseVersion) {
-        return "[ci skip] New release for v${nextReleaseVersion}"
-    }
-
-    String geBumpMessage(def nextDevelVersion) {
-        return "[ci skip] Version bump ${nextDevelVersion}"
-    }
-
-    String getGroupId() {
-        return this.script.sh(script: 'mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout', returnStdout: true).trim()
-    }
-
-    String getArtifactId() {
-        return this.script.sh(script: 'mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true).trim()
-    }
-
-    String getNextVersion() {
-        if(projectVersion == null) {
-            projectVersion = this.script.sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
-            projectVersion = projectVersion.replaceAll('-SNAPSHOT', '').trim()
-        }
-    
-        def versionParts = projectVersion.tokenize('.');  
-        def mayorVersionPart = versionParts[0];  
-        def minorVersionPart = versionParts[1];  
-        def fixVersionPart = versionParts[2]; 
-        
-        int fixVersion = fixVersionPart.toInteger()  + 1;  
-        
-        def developmentVersion = mayorVersionPart  + "." + minorVersionPart + "." + fixVersion + '-SNAPSHOT';  
-    
-        return developmentVersion
-    }
-
-    String getReleaseVersion() {
-        if(projectVersion == null) {
-            projectVersion = this.script.sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
-            projectVersion = projectVersion.replaceAll('-SNAPSHOT', '').trim()
-        }
-        return projectVersion
-    }
-
-    String getVersion() {
-         return this.script.sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
-    }
-
-    boolean isSkip() {
-        def result = this.script.sh (script: "git log -1 | grep '.*\\[ci skip\\].*'", returnStatus: true)
-        if(result == 0){
-            this.script.currentBuild.result = 'NOT_BUILT'
-            return true;
-        }
-        return false;
-
-    }
-
-    String getSonarProject(def name) {
-        if(name == null || name.isEmpty()){
-           name = this.getGroupId() + "-" + this.getArtifactId()
-        }
-        def branchName = this.script.env.BRANCH_NAME;
-
-        if(this.isTag()){
-            branchName = "tag/" + this.script.env.TAG_NAME;
-        }
-
-        return "-Dsonar.branch.name=\"${branchName}\" -Dsonar.projectName=\"${name}\" -Dsonar.projectKey=\"${name}\""
-    }
-
-    String getSonarProjectId(def name) {
-        if(name == null || name.isEmpty()){
-           name = this.getGroupId() + "-" + this.getArtifactId()
-        }
-        return name
-    }
+	private def currentVersion
+	private def projectVersion
+	private def artifactId
+	private def groupId
+	private def projectName
 	
+	Helper(def script, def projectName) {
+		this.script = script
+		this.projectName = projectName
+	}
+
+	def init() {
+		this.currentVersion = this.script.sh(script: 'mvn -B help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
+
+		this.projectVersion = this.currentVersion.replaceAll('-SNAPSHOT', '').trim()
+
+		this.artifactId = this.script.sh(script: 'mvn -B help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true).trim()
+
+		this.groupId = this.script.sh(script: 'mvn -B help:evaluate -Dexpression=project.groupId -q -DforceStdout', returnStdout: true).trim()
+		
+		if(this.projectName == null || this.projectName.isEmpty()){
+			this.projectName = this.groupId + "-" + this.artifactId
+		}
+	}
+	
+	boolean isPr() {
+		return this.script.env.BRANCH_NAME != null && this.script.env.BRANCH_NAME.startsWith("PR-");
+	}
+
+	boolean isTag(){
+		return this.script.env.TAG_NAME != null && !this.script.env.TAG_NAME.isEmpty()
+	}
+
+	boolean isBranch(){
+		return this.script.env.BRANCH_NAME != null && !this.script.env.BRANCH_NAME.isEmpty()
+	}
+
+	boolean isRelease(){
+		return (this.script.env.BRANCH_NAME == "develop" || this.script.env.BRANCH_NAME == "master" || this.script.env.BRANCH_NAME == "hotfix")
+	}
+
+	boolean isPublish(){
+		return this.isRelease() || this.isTag()
+	}
+
+	boolean isSnapshot(){
+		return !(this.isRelease() || this.isPublish())
+	}
+
+	boolean hasNoFailures(){
+		return  (this.script.currentBuild.result == null || this.script.currentBuild.result == 'SUCCESS')
+	}
+
+	String getReleaseMessage(def nextReleaseVersion) {
+		return "[ci skip] New release for v${nextReleaseVersion}"
+	}
+
+	String geBumpMessage(def nextDevelVersion) {
+		return "[ci skip] Version bump ${nextDevelVersion}"
+	}
+
+	String getGroupId() {
+		return this.groupId
+	}
+
+	String getArtifactId() {
+		return this.artifactId
+	}
+
+	String getNextVersion() {
+		def versionParts = this.projectVersion.tokenize('.');
+		int fixVersion = versionParts[versionParts.size -1].toInteger()  + 1;
+		versionParts[versionParts.size -1] = "${fixVersion}"
+		return "${versionParts.join('.')}-SNAPSHOT"
+	}
+
+	String getReleaseVersion() {
+		return this.projectVersion
+	}
+
+	String getVersion() {
+		return this.currentVersion
+	}
+
+	// If last commit message starts with [ci skip], we skip the build
+	boolean isSkip() {
+		def result = this.script.sh (script: "git log -1 | grep '.*\\[ci skip\\].*'", returnStatus: true)
+		if(result == 0){
+			this.script.currentBuild.result = 'NOT_BUILT'
+			return true;
+		}
+		return false;
+	}
+
+	String getSonarProject() {
+		def branchName = this.script.env.BRANCH_NAME;
+
+		if(this.isTag()){
+			branchName = "tag/" + this.script.env.TAG_NAME;
+		}
+
+		if(this.isPr()) {
+			def prBranch = this.script.env.CHANGE_BRANCH
+			def prKey = this.script.env.CHANGE_ID
+			def prBase = this.script.env.CHANGE_TARGET
+			
+			return "-Dsonar.pullrequest.key=\"${prKey}\" -Dsonar.pullrequest.branch=\"${prBranch}\" -Dsonar.pullrequest.base=\"${prBase}\" -Dsonar.projectName=\"${this.projectName}\" -Dsonar.projectKey=\"${this.projectName}\""
+		} else {
+			return "-Dsonar.branch.name=\"${branchName}\" -Dsonar.projectName=\"${this.projectName}\" -Dsonar.projectKey=\"${this.projectName}\""
+		}
+	}
+
+	String getSonarProjectId() {
+		return this.projectName
+	}
+
 	def qualityGate(def project, def branch, def timeOut, def ignoreSonarErrors = false){
 		if(! ignoreSonarErrors) {
 			this.steps.timeout(time: timeOut, unit: 'MINUTES') {
@@ -124,15 +129,15 @@ class Helper implements Serializable {
 				}
 			}
 		}
-  }
-  
+	}
+
 	def testReport(){
 		this.script.jacoco(execPattern: '**/*.exec')
-
+		
 		if(this.isPublish()){
 			this.script.tar file: 'reports-unit-tests.tar', archive: true, overwrite: true, glob: '**/surefire-reports/*'
 			this.script.tar file: 'reports-integration-tests.tar', archive: true, overwrite: true, glob: '**/failsafe-reports/*'
-		
+
 			this.script.tar file: 'reports-javadoc.tar', archive: true, overwrite: true, glob: './target/site/apidocs/**/*'
 			this.script.tar file: 'reports-jacoco.tar', archive: true, overwrite: true, glob: './target/site/jacoco/**/*'
 		}
@@ -145,6 +150,33 @@ class Helper implements Serializable {
 		} catch(Exception ex) {
 			steps.println "Publish dependency check findings got exception " + ex
 		}
-   }
+		this.script.allure results: [
+			[path: 'target/allure-results']
+		]
+	}
+	
+	def updateJira(def projectKey, def buildVersion) {
+		def versionParts = this.projectVersion.tokenize('.');
+		versionParts.remove(versionParts.size -1);
+
+		def jiraRelease = "${versionParts.join('.')}"
+		
+		// Ensure the release version exists in the project
+		this.script.jiraProjectRelease projectKey: projectKey, releaseVersion: jiraRelease
+		
+		// Find all issues either from commit messages or from the branch name itslef. Limit them to those of the 
+		// specific project
+		def jiraIssues = this.script.jiraListIssues branchName: this.script.env.BRANCH_NAME, projectKey: projectKey
+		
+		
+		jiraIssues.each { issue ->
+			this.script.echo "Found issue ${issue}, setting build version: ${buildVersion}, release: ${jiraRelease}"
+			// Update issue in project by setting the fixVersion and the build version (custom field)
+			this.script.jiraFixVersion projectKey: projectKey, buildVersionFieldId: 'customfield_11765', issueKey: issue, buildVersion: buildVersion, releaseVersion: jiraRelease
+			//this.script.jiraAddComment comment: "{panel:bgColor=#97FF94}{code}Code was added to address this issue in build ${build}{code} {panel}", idOrKey: issue, site: jiraServer
+			//def fixedInBuild = [fields: [customfield_10121: build]] // This is a custom field named "Fixed in Build"
+			//this.script.jiraEditIssue idOrKey: issue, issue: fixedInBuild, site: jiraServer
+		}
+	}
 }
 
--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -8,7 +8,6 @@ def call(Closure body) {
     body.delegate = pipelineParams
     body()
 
-    def helper = new Helper(this)
     def nextDevelVersion = '';
     def nextReleaseVersion = '';
     def release = false;
@@ -17,14 +16,17 @@ def call(Closure body) {
     def skip = false;
     def sonarProject = "";
     def projectName = pipelineParams.name;
+	def jiraProject = pipelineParams.jiraProject;
     def ignoreSonarErrors = false;
     def sonarProjectName;
        
+	def helper = new Helper(this, projectName)
+	
     pipeline {
 
         agent any
         tools { 
-            maven 'MVN3_8' 
+            maven 'MVN' 
             jdk 'JDK11' 
         }
         options {
@@ -35,22 +37,28 @@ def call(Closure body) {
             stage('Init') {
                 steps {
                     script {
+						helper.init()
+
                         release = helper.isRelease();
                         snapshot = helper.isSnapshot();
                         publish = helper.isPublish();
                         
+						// next devel version with -SNAPSHOT
                         nextDevelVersion = helper.getNextVersion()
+						// version without -SNAPSHOT (if any)
                         nextReleaseVersion = helper.getReleaseVersion()
 
+						// check if we should skip this build
                         skip = helper.isSkip();
-                        sonarProject = helper.getSonarProject(projectName)
-                        sonarProjectName = helper.getSonarProjectId(projectName);
-                        
+						// sonar related 
+                        sonarProject = helper.getSonarProject()
+                        sonarProjectName = helper.getSonarProjectId();
                         if(pipelineParams.ignoreSonarErrors != null){
                            ignoreSonarErrors = pipelineParams.ignoreSonarErrors 
                         }
                     }
-                    sh "echo ${nextDevelVersion} ${nextReleaseVersion} publish=${publish} release=${release} snapshot=${snapshot} skip=${skip}, projectName=${projectName} sonarProject=${sonarProject}"
+                    echo "Project info: nextDevelVersion=${nextDevelVersion} releaseVersion=${nextReleaseVersion} publish=${publish} release build=${release} snapshot=${snapshot} skip=${skip}, projectName=${projectName} sonarProject=${sonarProject}"
+                    sh "export"
                 }
             }
             stage("Build and Publish") {
@@ -63,7 +71,8 @@ def call(Closure body) {
                             expression { release == true }
                         }
                         steps {
-                            sh "mvn -B versions:set -DnewVersion=${nextReleaseVersion} -DprocessAllModules=true -Pittests"
+							// Remove -SNAPSHOT from poms as this is a release 
+                            sh "mvn -B versions:set versions:set-property -Dproperty=revision -DgenerateBackupPoms=false -DnewVersion=${nextReleaseVersion} -DprocessAllModules=true -Pittests -Pcicd"
                         }
                     }
 
@@ -74,7 +83,6 @@ def call(Closure body) {
                                 if (fileExists('suppressions.xml')) {
                                     extraBuildArgs += " -DsuppressionFile=suppressions.xml"
                                 }
-
                                 sh "mvn -B clean verify -U -Dmaven.test.failure.ignore=true -Pcicd -Dsnapshot.build=${snapshot} ${extraBuildArgs}"
                                 helper.testReport()
                             }
@@ -83,7 +91,7 @@ def call(Closure body) {
                     stage('SonarQube analysis') {
                         steps {
                              script {
-                                withSonarQubeEnv('Sonarqube') {
+                                withSonarQubeEnv('sonar') {
                                     sh "mvn -B -Pcicd sonar:sonar ${sonarProject} -Dsonar.exclusions=**/pom.xml  -Dsonar.dependencyCheck.jsonReportPath=target/dependency-check-report.json -Dsonar.dependencyCheck.xmlReportPath=target/dependency-check-report.xml -Dsonar.dependencyCheck.htmlReportPath=target/dependency-check-report.html"
                                 }
                                 sleep (25)
@@ -97,7 +105,10 @@ def call(Closure body) {
                             expression { publish == true && helper.hasNoFailures()}
                         }
                         steps {
-                            sh "mvn -B deploy -DskipTests=true"
+							// Deploy jars in maven repository and images in registry
+                            echo "We don't deloy yet, do a local install"
+                            //sh "mvn -B deploy -DskipTests=true"
+                            sh "mvn -B install -DskipTests=true"
                         }
                     }
 
@@ -110,23 +121,24 @@ def call(Closure body) {
                                 releaseMsg  = helper.getReleaseMessage(nextReleaseVersion);
                                 bumpMsg  = helper.geBumpMessage(nextDevelVersion);
                                 currentBuild.displayName = "v" + nextReleaseVersion +  " (" + currentBuild.number + ")"
-
+								
                                 sh 'git config advice.addEmptyPathspec false'
-                                sh "mvn -B versions:commit -Pittests"
-                                withCredentials([
-                                    gitUsernamePassword(credentialsId: 'GIT_CREDENTIALS', gitToolName: 'Default')
-                                ]) {
+								
+                                withCredentials([gitUsernamePassword(credentialsId: 'alpapad-jenkins-ap', gitToolName: 'Default')]) {
                                     sh "git ls-files -m | grep pom.xml  | xargs git add"
                                     sh "git commit -m \"${releaseMsg}\" --allow-empty"
-                                    sh "git tag -a v${nextReleaseVersion} -m \"${releaseMsg}\""
-                                    sh "git push origin HEAD:develop --tags"
-    
-                                    sh "mvn -B versions:set -DnewVersion=${nextDevelVersion} -DprocessAllModules=true"
+                                    sh "git tag -f -a v${nextReleaseVersion} -m \"${releaseMsg}\""
+									
+                                    sh "mvn -B versions:set versions:set-property -Dproperty=revision -DgenerateBackupPoms=false -DnewVersion=${nextDevelVersion} -DprocessAllModules=true -Pittests -Pcicd"
     
                                     sh "git ls-files -m | grep pom.xml  | xargs git add"
                                     sh "git commit -m \"${bumpMsg}\" --allow-empty"
-                                    sh "git push origin HEAD:develop --tags"
-                                   }
+                                    //sh "git push origin HEAD:develop --tags"
+									sh "git push origin HEAD --tags"
+									//git push origin HEAD:refs/tags/alex2
+                                }
+								// Update relevant jira issues with fix versions and build versions
+								helper.updateJira(jiraProject,nextReleaseVersion)
                             }
                         }
                     }
