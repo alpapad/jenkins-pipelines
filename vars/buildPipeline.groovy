16cf8b397a356706d1964b1f3f744c0b8a0d763b
alpapad01 <133748444+alpapad01@users.noreply.github.com>
2024-02-13 21:47:52 +0200

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -96,7 +96,7 @@ def call(Closure body) {
                                     if (fileExists('suppressions.xml')) {
                                         extraBuildArgs += " -DsuppressionFile=suppressions.xml"
                                     }
-                                    sh "mvn -B clean verify -U -Dmaven.test.failure.ignore=true -Pcicd -Dsnapshot.build=${snapshot} ${extraBuildArgs}"
+                                    sh "mvn -B clean verify -U -Dmaven.test.failure.ignore=true -Pdocker-build -Pcicd -Dsnapshot.build=${snapshot} ${extraBuildArgs}"
                                     helper.junitReport()
                                 }
                             }
@@ -113,7 +113,7 @@ def call(Closure body) {
                                         sh "docker compose down -v| true"
                                         sh "docker compose rm | true"
                                         sh "docker volume prune -a -f | true"
-                                        sh "mvn clean install -Pdocker-build -DskipTests=true"
+                                        //sh "mvn clean install -Pdocker-build -DskipTests=true"
                                         sh "SPRING_PROFILES=docker,jenkins docker compose up -d"
                                         sleep(90)
                                         sh "mvn -B verify -pl testing/smg-it-tests -am -Dmaven.test.failure.ignore=true -Ddependency-check.skip=true -Pit-tests -Pcicd -Dspring.profiles.active=jenkins"
